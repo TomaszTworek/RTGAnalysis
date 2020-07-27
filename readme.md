8c166d8489784919fa67a2af3de452f3058d4b8c
Tomasz Tworek <tomasztworek27@gmail.com>
2020-07-27 21:46:09 +0200

    Initial commit, add .gitignore, readme file and configuration

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Project Title
+
+This is a desktop application for RTG analysis. 
+User may mark multiple points on the X-ray image to point out sensitive areas. 
+
+## Built With
+
+* [JavaFx](https://openjfx.io/) - Application platform for desktop
+* [Maven](https://maven.apache.org/) - Dependency Management
---
+++ src/main/java/pl/rekrutacja/rtg/Main.java
@@ -0,0 +1,17 @@
+package pl.rekrutacja.rtg;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("RTG Analysis");
+        primaryStage.show();
+    }
+}
