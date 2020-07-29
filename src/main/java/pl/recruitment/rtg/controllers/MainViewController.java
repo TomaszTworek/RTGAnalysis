70ca7fe069b1115222abe56cded3db0758843641
Tomasz Tworek <tomasztworek27@gmail.com>
2020-07-29 17:06:26 +0200

    #03 Create basic images view in gridPane and define methods to add points in the future

--- src/main/java/pl/recruitment/rtg/controllers/MainViewController.java
+++ src/main/java/pl/recruitment/rtg/controllers/MainViewController.java
@@ -1,4 +1,16 @@
 package pl.recruitment.rtg.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
 public class MainViewController {
+
+    @FXML
+    public VBox pointsVBox;
+
+    @FXML
+    public void addNewPoint(MouseEvent mouseEvent) {
+
+    }
 }
