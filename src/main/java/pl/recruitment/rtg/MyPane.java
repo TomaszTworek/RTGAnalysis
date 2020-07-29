8b9a593479123843550b018581c748e413510302
Tomasz Tworek <tomasztworek27@gmail.com>
2020-07-29 19:33:09 +0200

    @03 Add scroll pane, to avoid problem with many points

--- src/main/java/pl/recruitment/rtg/MyPane.java
+++ src/main/java/pl/recruitment/rtg/MyPane.java
@@ -1,5 +1,6 @@
 package pl.recruitment.rtg;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
 
--- src/main/java/pl/recruitment/rtg/controllers/MainViewController.java
+++ src/main/java/pl/recruitment/rtg/controllers/MainViewController.java
@@ -1,7 +1,11 @@
 package pl.recruitment.rtg.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import pl.recruitment.rtg.MyPane;
 
@@ -11,9 +15,19 @@ public class MainViewController {
     public VBox pointsVBox;
 
     @FXML
+    public ScrollPane scrollPoints;
+
+    @FXML
     public void addNewPoint(MouseEvent mouseEvent) {
         createPointPaneViewAndAddToVBox();
     }
+
+/*    @FXML
+    public void initialize() {
+        for (Node p : pointsVBox.getChildren()) {
+            VBox.setVgrow(p, Priority.ALWAYS);
+        }
+    }*/
 
     private void createPointPaneViewAndAddToVBox() {
         MyPane pointPane = new MyPane();
