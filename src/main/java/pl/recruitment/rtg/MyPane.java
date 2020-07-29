81ac3dfb9a771c147a15b02ec7ea77fd5a572dbd
Tomasz Tworek <tomasztworek27@gmail.com>
2020-07-29 18:27:56 +0200

    #01 Add points view representation to Vbox by clicking on image

---
+++ src/main/java/pl/recruitment/rtg/MyPane.java
@@ -0,0 +1,20 @@
+package pl.recruitment.rtg;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class MyPane extends Pane {
+
+    public MyPane() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/Point.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
--- src/main/java/pl/recruitment/rtg/controllers/MainViewController.java
+++ src/main/java/pl/recruitment/rtg/controllers/MainViewController.java
@@ -3,6 +3,7 @@ package pl.recruitment.rtg.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import pl.recruitment.rtg.MyPane;
 
 public class MainViewController {
 
@@ -11,6 +12,11 @@ public class MainViewController {
 
     @FXML
     public void addNewPoint(MouseEvent mouseEvent) {
+        createPointPaneViewAndAddToVBox();
+    }
 
+    private void createPointPaneViewAndAddToVBox() {
+        MyPane pointPane = new MyPane();
+        pointsVBox.getChildren().add(pointPane);
     }
 }
