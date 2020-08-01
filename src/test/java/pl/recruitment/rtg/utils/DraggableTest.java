b340187ac3f3760f9b8d16d3ff45f59121d65f82
Tomasz Tworek <tomasztworek27@gmail.com>
2020-08-01 14:48:15 +0200

    #03 Add assertJ and test for Draggable

---
+++ src/test/java/pl/recruitment/rtg/utils/DraggableTest.java
@@ -0,0 +1,27 @@
+package pl.recruitment.rtg.utils;
+
+import javafx.scene.Node;
+import javafx.scene.shape.Circle;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class DraggableTest {
+
+    private Draggable.Nature draggable;
+
+    @Test
+    public void shouldAddNode(){
+        //given
+        Node rootNode = new Circle();
+        Node nodeToAdd = new Circle();
+        draggable = new Draggable.Nature(rootNode);
+
+        //when
+        draggable.addDraggedNode(nodeToAdd);
+
+        //then
+        Assertions.assertThat(draggable.getDragNodes().size()).isEqualTo(2);
+    }
+    
+}
